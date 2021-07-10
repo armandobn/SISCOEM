8b4f25a2473384297a4411936369a1d98d632a11
Araceli Cabello <81246309+AraceliCabello18@users.noreply.github.com>
2021-07-10 18:33:20 -0500

    vista de usuarios

---
+++ vista_usuarios.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="librerias/jquery-3.5.1.js">
+    <link rel="stylesheet" type="text/css" href="librerias/jquery.dataTables.min.css">
+    <script type="text/javascript" src="librerias/jquery.dataTables.min.js"></script>
+    <title>Ejemplo de nueva página para GOB.mx</title>
+
+    <!-- CSS -->
+    <link href="/favicon.ico" rel="shortcut icon">
+    <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+
+
+
+    <!-- Respond.js soporte de media queries para Internet Explorer 8 -->
+    <!-- ie8.js EventTarget para cada nodo en Internet Explorer 8 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Contenido -->
+    <main class="page">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <br>
+                    <br>
+                    <h1 style="text-align:center;" class="lead">Proceso Transcurrido</h1>
+                    <br>
+                    <h4 style="text-align:center;" class="lead">Tiempo Transcurrido</h4>
+                    <hr>
+                    <table id="example" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                        <tr>
+                            <td>Plantel</td>
+                            <td>Area Auxiliar</td>
+                            <td>DGETI</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- JS -->
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
+    <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+    <script src="js/validar_index.js"></script>
+
+</body>
+
+</html>
