f83ccdab899d676c375b11b987bf3cfba49db12a
Araceli Cabello <81246309+AraceliCabello18@users.noreply.github.com>
2021-07-10 17:31:32 -0500

    cambios de validacion

---
+++ js/validar_index.js
@@ -0,0 +1,32 @@
+var btn = document.getElementById('Enviar');
+var clave = document.getElementById('password');
+var usuario = document.getElementById('usuario');
+
+
+
+btn.addEventListener('click', function(evt){
+
+      if(clave.value === ''){
+
+          console.log('el campo contraseña es obligatorio')
+          evt.preventDefault();
+          return false;
+
+      }else if(usuario.value === ''){
+
+      console.log('el campo de usuario es obligatorio')
+          evt.preventDefault();
+          return false;
+
+      }else if(usuario.value.length > 30){
+
+        console.log('El nombre del usuario es demasiado largo')
+          evt.preventDefault();
+          return false;
+
+      }
+
+
+
+
+});
