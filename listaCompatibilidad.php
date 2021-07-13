88489feefb9abc42fbb64befba0155014341ff17
Araceli Cabello <81246309+AraceliCabello18@users.noreply.github.com>
2021-07-13 17:18:58 -0500

    corresion

--- listaCompatibilidad.php
+++ listaCompatibilidad.php
@@ -23,69 +23,68 @@
 <body>
 
     <!-- Contenido -->
-    <main class="page">
         <div class="container">
             <div class="row">
                 <div class="col">
                     <br>
                     <br>
-                    <h1 style="text-align:center;" class="lead">Personal Docente/ Administratrivo con Horas Compativilidad de Empleos</h1>
+                    <h1 style="text-align:center;" class="lead">Personal Docente/ Administratrivo con Horas
+                        Compativilidad de Empleos</h1>
                     <br>
                     <form id="frmformulario" method="POST" class="form-row" role="form" onsubmit="return logear()">
-                        <div class="col-md-4 mb-3">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" id="nombre" placeholder="Nombre">
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="tmm">Tipo de MOV-MOT.:</label>
-                            <input type="text" class="form-control" id="tmm" placeholder="Tipo de MOV - MOT">
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="ct">C.T:</label>
-                            <input type="text" class="form-control" id="Ct" placeholder="C.T">
-                        </div>
-                        <br>
-                        <div class="col-md-4 mb-3">
-                            <label for="filiacion">Filiacion:</label>
-                            <input type="text" class="form-control" id="filiacion" placeholder="Filiacion">
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="ctcb">CETis o CBTis No:</label>
-                            <input type="text" class="form-control" id="ctcb" placeholder="CETis o CBTis No">
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="fecha">Fecha:</label>
-                            <input type="text" class="form-control" id="fecha" placeholder="Fecha">
-                        </div>
-                        <br>
-                        <div class="col-md-4 mb-2">
-                            <label for="efectos">Efectos:</label>
-                            <input type="text" class="form-control" id="efectos" placeholder="Efectos">
-                        </div>
-                        <div class="col-md-4 mb-2">
-                            <label for="siapsepweb">No.Doc SIAPSEPWEB:</label>
-                            <input type="text" class="form-control" id="siapsepweb" placeholder="No.Doc SIAPSEPWEB">
-                        </div>
-                        <br>
-                        <div class="col-md-4 mb-2">
-                            <label for="clave">Clave(s):</label>
-                            <input type="text" class="form-control" id="clave" placeholder="Clave(s)">
-                        </div>
-                        <div class="col-md-4 mb-2">
-                            <label for="concecutivo">No.Consecutivo:</label>
-                            <input type="text" class="form-control" id="concecutivo" placeholder="No.Consecutivo">
-                        </div>
-                        <br>
-                        <br>
+                    <div class="col-md-4 mb-3">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="tmm">Tipo de MOV-MOT.:</label>
+                        <input type="text" class="form-control" id="tmm" placeholder="Tipo de MOV - MOT">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="ct">C.T:</label>
+                        <input type="text" class="form-control" id="Ct" placeholder="C.T">
+                    </div>
+                    <br>
+                    <div class="col-md-4 mb-3">
+                        <label for="filiacion">Filiacion:</label>
+                        <input type="text" class="form-control" id="filiacion" placeholder="Filiacion">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="ctcb">CETis o CBTis No:</label>
+                        <input type="text" class="form-control" id="ctcb" placeholder="CETis o CBTis No">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="fecha">Fecha:</label>
+                        <input type="text" class="form-control" id="fecha" placeholder="Fecha">
+                    </div>
+                    <br>
+                    <div class="col-md-4 mb-2">
+                        <label for="efectos">Efectos:</label>
+                        <input type="text" class="form-control" id="efectos" placeholder="Efectos">
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="siapsepweb">No.Doc SIAPSEPWEB:</label>
+                        <input type="text" class="form-control" id="siapsepweb" placeholder="No.Doc SIAPSEPWEB">
+                    </div>
+                    <br>
+                    <div class="col-md-4 mb-2">
+                        <label for="clave">Clave(s):</label>
+                        <input type="text" class="form-control" id="clave" placeholder="Clave(s)">
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="concecutivo">No.Consecutivo:</label>
+                        <input type="text" class="form-control" id="concecutivo" placeholder="No.Consecutivo">
+                    </div>
+                   <br>
+
                     </form>
                 </div>
             </div>
         </div>
-    </main>
     <br>
     <div class="container">
-            <div class="row">
-                <div class="col">
+        <div class="row">
+            <div class="col">
                 <form id="frmlista" method="POST" class="form-row" role="form" onsubmit="return logear()">
                     <table class="table table-bordered">
                         <thead>
@@ -141,44 +140,49 @@
                             </tr>
                         </tbody>
                     </table>
-                </form>  
-                </div>
+                </form>
             </div>
         </div>
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                <form id="frmlogin" method="POST" class="form-horizontal" role="form" onsubmit="return logear()">
-            <div class="form-group">
-              <label class="col-sm-3 control-label" for="nombre">Nombre:</label>
-              <div class="col-sm-9">
-                <input class="form-control" id="nombre" placeholder="Ingresa nombre" type="text">
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="col-sm-3 control-label" for="fecha">Fecha:</label>
-              <div class="col-sm-9">
-                <input class="form-control" id="fecha" placeholder="fecha" type="text">
-              </div>
-              </div>
-              <div class="form-group">
-              <label class="col-sm-3 control-label" for="firma">Firma:</label>
-              <div class="col-sm-9">
-                <input class="form-control" id="firma" placeholder="firma" type="text">
-              </div>
-              </div>
-                </form>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+                <div class="card">
+                    <h5 class="card-header">Director</h5>
+                    <div class="card-body">
+                    <form id="frmlogin" method="POST" class="form-horizontal" role="form" onsubmit="return logear()">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="nombre">Nombre:</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" id="nombre" placeholder="Ingresa nombre" type="text">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="fecha">Fecha:</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" id="fecha" placeholder="fecha" type="text">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="firma">Firma:</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" id="firma" placeholder="firma" type="text">
+                        </div>
+                    </div>
+                </form>  
+                    </div>
+                </div>
                 <br>
-                <button class="btn btn-primary" type="submit">Completado</button> 
+                <button class="btn btn-primary" type="submit">Completado</button>
+                <br>
             </div>
         </div>
 
-    <!-- JS -->
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
-    <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
-    <script src="js/validar_index.js"></script>
+        <!-- JS -->
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
+        <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+        <script src="js/validar_index.js"></script>
 
 </body>
 
