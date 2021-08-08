1ead52f54d2a495088f8c8e7d69be744a706e163
armandobn <72363199+armandobn@users.noreply.github.com>
2021-08-08 18:31:09 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         $this->app->bind('path.public', function(){
-            return '/home2/simdirne/siscoem.ddns.net/public';
+            return '/home2/simdirne/siscoem.ddns.net/public/';
         });
     }
 
