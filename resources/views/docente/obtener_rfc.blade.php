df99b94f924a8c42e29cdf7db4d68e15f1d02980
armandobn <72363199+armandobn@users.noreply.github.com>
2021-08-18 18:55:31 -0500

    cambios Docente

--- resources/views/docente/obtener_rfc.blade.php
+++ resources/views/docente/obtener_rfc.blade.php
@@ -24,19 +24,45 @@
 
 <form action="{{route('actualizarDocente.obtenerRfc')}}" method="post" id="form_buscar_rfc">
     @csrf
-    <div class="row">
-        <div class="col-md-4">
-            <div class="form-group">
-                <label class="control-label" for="buscar_rfc">R.F.C
-                    <span class="form-text" id="asterisco_buscar_rfc">*</span>:
-                </label>
-                <input class="form-control" id="buscar_rfc" name="buscar_rfc" placeholder="Ingrese R.F.C" type="text">
-            </div>
-            <div class="pull-right">
-                <button type="button" class="btn btn-primary" id="btn_buscar_rfc">Buscar</button>
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#tab-01"> Buscar por RFC</a></li>
+        <li><a data-toggle="tab" href="#tab-04">Buscar por nombre completo</a></li>
+      </ul>
+      <div class="tab-content">
+        <div class="tab-pane active" id="tab-01">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="control-label" for="buscar_rfc">Buscar por R.F.C
+                            <span class="form-text" id="asterisco_buscar_rfc">*</span>:
+                        </label>
+                        <input class="form-control" id="buscar_rfc" name="buscar_rfc" placeholder="Ingrese R.F.C" type="text">
+                    </div>
+                    <div class="pull-right">
+                        <button type="button" class="btn btn-primary" id="btn_buscar_rfc">Buscar</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+        <div class="tab-pane" id="tab-04">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="control-label" for="buscar_rfc">Buscar por nombre completo
+                            <span class="form-text" id="asterisco_buscar_rfc">*</span>:
+                        </label>
+                        <input class="form-control" id="" name="" placeholder="Ingrese nombre completo" type="text">
+                    </div>
+                    <div class="pull-right">
+                        <button type="button" class="btn btn-primary" id="">Buscar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+
+    
 </form> 
 
 @endsection
