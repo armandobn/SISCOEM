44e41ca05270688d4489e86e63debbfa5aa95857
sanngam <65148140+sanngam@users.noreply.github.com>
2021-12-14 19:09:11 -0600

    creacion de proyecto

--- controllers/login.php
+++ controllers/login.php
@@ -1,1 +1,24 @@
+<?php 
 
+  session_start();
+  require_once("../database/database.php");
+  require_once("../views/login.php");
+
+  $objeto_conexion = conexion();
+
+  $email = $_POST['email'];
+  $password = sha1($_POST['password']);
+
+
+  $query_de_busqueda = "SELECT * FROM logins WHERE email='$email' and password='$password'";
+
+  $resultado_del_query= mysqli_query($objeto_conexion, $query_de_busqueda);
+
+  if(mysqli_num_rows($resultado_del_query) > 0){
+    $_SESSION['logins']=$email;
+    echo 1;
+  }else{
+    echo 0;
+  }
+  
+?>
--- controllers/registroLogin.php
+++ controllers/registroLogin.php
@@ -1,1 +1,19 @@
+<?php
+require_once("../database/database.php");
+require_once("../views/registroLogin.php");
+?>
 
+$objeto_para_usar_conexion = conexion();
+
+$nombre = $_POST['nombre'];
+$apePaterno = $_POST['apePaterno'];
+$apeMaterno = $_POST['apeMaterno'];
+$email_= $_POST['email'];
+$password = sha1($_POST['password']);
+
+
+$query_insert_usuario = "insert into usuarios(nombre, apePaterno, apeMaterno, email, password) 
+                                        values('$nombre', '$apePaterno', '$apeMaterno', '$email', '$password')";
+
+echo $resultado_de_la_ejecucion_del_query = mysqli_query($objeto_para_usar_conexion, $query_insert_usuario);
+?>
